398090c7eeda461d07952c50067196ab32812ed8
Harvey Robinson <hr16561@my.bristol.ac.uk>
2019-01-22 16:45:56 +0000

    Add personalised Intructions properly

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,54 +1,141 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
 
-public class GameController : NetworkBehaviour {
+public class GameController : NetworkBehaviour
+{
 
     public Text scoreText, instruction1, instruction2, instruction3, instruction4;
-    
-    [SyncVar]
-    public int score = 0;
 
+    [SyncVar] public int score = 0;
     SyncListString activeInstructions = new SyncListString();
 
-    public int IDcount = 1;
+    private List<String> burgerRecipe = new List<string>();
+    // private List<String> otherRecipe; 
+
+    private Stack<String> random = new Stack<string>();
+    private Stack<String> ordered = new Stack<string>();
+    private int numberOfButtons = 4;
+
+    public List<Player> playerList = new List<Player>();
+
+//    private void ChooseRecipe()
+//    {
+//        //This is where we choose recipe
+//        burgerRecipe.Add("Grab Meat");
+//        burgerRecipe.Add("Grab Salad");
+//        burgerRecipe.Add("Grab Buns");
+//        burgerRecipe.Add("Grab Cheese");
+//        burgerRecipe.Add("Grind Meat");
+//        burgerRecipe.Add("Chop Salad");
+//        burgerRecipe.Add("Cut Bun");
+//        burgerRecipe.Add("Wash Salad");
+//
+//    }
+
+    //return random Stack as randomly ordered stack of instructions
+    private Stack<String> setRandom(List<String> list)
+    {
+        System.Random rnd = new System.Random();
+        for (int i = 0; i < list.Count; i++)
+        {
+            int k = rnd.Next(0, i);
+            String value = list[k];
+            list[k] = list[i];
+            list[i] = value;
+        }
+
+        return new Stack<String>(list);
+    }
+
+    //Return
+    private Stack<String> setOrdered(List<String> list)
+    {
+        list.Reverse();
+        return new Stack<String>(list);
+    }
+
 
 
     void Start()
     {
+
+
         //Show server display only on the server.
         if (isServer) GetComponentInChildren<Canvas>().enabled = true;
-//Test
+
+//        ChooseRecipe();
+        burgerRecipe.Add("Grab Meat");
+        burgerRecipe.Add("Grab Salad");
+        burgerRecipe.Add("Grab Buns");
+        burgerRecipe.Add("Grab Cheese");
+        burgerRecipe.Add("Grind Meat");
+        burgerRecipe.Add("Chop Salad");
+        burgerRecipe.Add("Cut Bun");
+        burgerRecipe.Add("Wash Salad");
+
+
+        activeInstructions.Add(burgerRecipe[0]);
+        activeInstructions.Add(burgerRecipe[1]);
+
+        ordered = setOrdered(burgerRecipe);
+        random = setRandom(burgerRecipe);
+
+
+
+
+
         //Assign actions to each player.
         var players = FindObjectsOfType<Player>();
-        foreach (Player p in players) {
+
+        foreach (Player p in players)
+        {
+            playerList.Add(p);
             p.SetGameController(this);
-            p.SetActionButtons(IDcount);
-            IDcount++;
+            for (int i = 0; i < numberOfButtons; i++)
+            {
+                Console.WriteLine(random.Peek());
+                p.SetActionButtons(random.Pop(), i);
+
+            }
+
+            p.SetInstruction(ordered.Pop());
+
         }
+        //   playerList[0].SetInstruction(ordered.Pop());
+//       playerList[1].SetInstruction(ordered.Pop());
+        //  playerList[1].instructionText.text = ordered.Pop();
 
-        //Add instructions to list of active instructions.
-        activeInstructions.Add("Chop Carrot");
-        activeInstructions.Add("Fry Burger");
-        activeInstructions.Add("Scramble Eggs");
-        activeInstructions.Add("Heat Oven");
+
+        Debug.Log(activeInstructions[0]);
+        Debug.Log(activeInstructions[1]);
+
     }
-
 
     private void Update()
     {
         //Show score and active instructions on server display.
         scoreText.text = score.ToString();
-        instruction1.text = activeInstructions[0];
-        instruction2.text = activeInstructions[1];
-        instruction3.text = activeInstructions[2];
-        instruction4.text = activeInstructions[3];
+//        instruction1.text = activeInstructions[0];
+//        instruction2.text = activeInstructions[1];
+//        instruction3.text = activeInstructions[2];
+//        instruction4.text = activeInstructions[3];
     }
 
-    
+    [ClientRpc]
+    public void RpcUpdateInstructions(String newInstruction, int playerID)
+    {
+        playerList[playerID].SetInstruction(newInstruction);
+    }
+
+
     [Server]
     public void CheckAction(string action)
     {
@@ -57,11 +144,11 @@ public class GameController : NetworkBehaviour {
         {
             //If match, increment score.
             if (action == activeInstructions[i])
-            { 
+            {
                 score++;
-                activeInstructions[i] = "COMPLETE!";
+                activeInstructions[i] = ordered.Peek();
+                RpcUpdateInstructions(ordered.Pop(), i);
             }
         }
     }
-    
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -10,13 +11,19 @@ public class Player : NetworkBehaviour {
 
     public Button button1, button2, button3, button4;
 
-    public Text scoreText;
+    public Text scoreText, instructionText;
+    
+//    public Button[] buttons = new Button[4];
 
 
     private void Start()
     {
         //Link Player GameObject to GameController.
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+//        buttons[0] = button1;
+//        buttons[1] = button2;
+//        buttons[2] = button3;
+//        buttons[3] = button4;
     }
 
 
@@ -33,25 +40,50 @@ public class Player : NetworkBehaviour {
     }
 
     //Assign instructions to each player (NOTE: Currently only works for up to 2 players).
-    public void SetActionButtons(int ID)
+    public void SetActionButtons(string instruction, int i)
     {
         if (isLocalPlayer)
         {
-            if (ID == 1)
+            switch (i)
             {
-                button1.GetComponentInChildren<Text>().text = "Chop Carrot";
-                button2.GetComponentInChildren<Text>().text = "Wash Dishes";
-                button3.GetComponentInChildren<Text>().text = "Blend Smoothie";
-                button4.GetComponentInChildren<Text>().text = "Fry Burger";
-            }
-            if (ID == 2)
-            {
-                button1.GetComponentInChildren<Text>().text = "Heat Oven";
-                button2.GetComponentInChildren<Text>().text = "Chop Onion";
-                button3.GetComponentInChildren<Text>().text = "Scramble Eggs";
-                button4.GetComponentInChildren<Text>().text = "Slice Cheese";
+                case 0:
+                    button1.GetComponentInChildren<Text>().text = instruction;
+                    break;
+                case 1:
+                    button2.GetComponentInChildren<Text>().text = instruction;
+                    break;
+                case 2:
+                    button3.GetComponentInChildren<Text>().text = instruction;
+                    break;
+                case 3:
+                    button4.GetComponentInChildren<Text>().text = instruction;
+                    break;
+                default:
+                    Console.WriteLine("ERROOOR");
+                    break;
             }
         }
+//        {
+//            if (ID == 1)
+//            {
+//                button1.GetComponentInChildren<Text>().text = "Chop Carrot";
+//                button2.GetComponentInChildren<Text>().text = "Wash Dishes";
+//                button3.GetComponentInChildren<Text>().text = "Blend Smoothie";
+//                button4.GetComponentInChildren<Text>().text = "Fry Burger";
+//            }
+//            if (ID == 2)
+//            {
+//                button1.GetComponentInChildren<Text>().text = "Heat Oven";
+//                button2.GetComponentInChildren<Text>().text = "Chop Onion";
+//                button3.GetComponentInChildren<Text>().text = "Scramble Eggs";
+//                button4.GetComponentInChildren<Text>().text = "Slice Cheese";
+//            }
+//        }
+    }
+
+    public void SetInstruction(String d)
+    {
+        instructionText.text = d;
     }
 
 
