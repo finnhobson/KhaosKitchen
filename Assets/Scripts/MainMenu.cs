a5024b86576fd0f4ce33249f3d99bf5bcb257378
Unknown <fh16413@my.bristol.ac.uk>
2019-01-30 18:57:46 +0000

    Demo working on laptop + 3 phones (no phone features)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -32,7 +32,7 @@ public class MainMenu : MonoBehaviour {
 
     public void OnClickJoin()
     {
-        lobbyManager.networkAddress = "127.0.0.1";
+        lobbyManager.networkAddress = "172.20.10.4";
         Debug.Log("Joining Game using IP Address: " + lobbyManager.networkAddress);
         lobbyManager.StartClient();
         Lobby.SetActive(true);
