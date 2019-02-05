39ff6015bc73e0617ba1c1ff0c8dee517a448540
Unknown <fh16413@my.bristol.ac.uk>
2019-02-05 15:17:02 +0000

    Started Working on Lobby UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -32,8 +32,8 @@ public class MainMenu : MonoBehaviour {
 
     public void OnClickJoin()
     {
-        lobbyManager.networkAddress = "192.168.0.26";
-        //lobbyManager.networkAddress = "127.0.0.1";
+        //lobbyManager.networkAddress = "192.168.0.26";
+        lobbyManager.networkAddress = "127.0.0.1";
         Debug.Log("Joining Game using IP Address: " + lobbyManager.networkAddress);
         lobbyManager.StartClient();
         Lobby.SetActive(true);
