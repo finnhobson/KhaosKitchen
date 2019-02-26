d665cb114490668eed07e9cc7f3990d1b0d6b302
Harry Goode <hg15150@my.bristol.ac.uk>
2019-02-26 14:22:45 +0000

    Created Instruction Class

---
+++ Assets/Scripts/Instruction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction
+{
+    public int ID { get; set; }
+    public string Action { get; set; }
+    public float Timer { get; set; }
+    public bool IsActive { get; set; }
+
+    public void printInstruction()
+    {
+        Debug.Log("ID: " + ID + "\nAction: " + Action + "\n Timer: " + Timer + "\n IsActive: " + IsActive);
+    }
+}
