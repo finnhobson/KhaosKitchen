06de797d045847023b67da4bd0c11733e7102576
Unknown <fh16413@my.bristol.ac.uk>
2019-05-13 21:29:00 +0100

    Fixed Camera Instructions

--- Assets/Scripts/InstructionController.cs
+++ Assets/Scripts/InstructionController.cs
@@ -38,7 +38,7 @@ public class InstructionController : NetworkBehaviour
                                                                                     "Pan set on fire!\nShake to put it out!\n\n(SHAKE YOUR PHONE)", "We are ten years away from irreversible climate damage!\n Shake to ignore\n\n(SHAKE YOUR PHONE)",
                                                                                     "Your arch nemisis just walked in!\nShake your fist at them angrily!\n\n(SHAKE YOUR PHONE)"});
 
-    private static List<String> cameraInstructionText = new List<string>(new string[] { "FIND THE ORANGE!", "FIND THE BANANAS!", "FIND THE APPLE!", "FIND THE EU FLAG TO REVOKE ARTICLE 50!" });
+    private static List<String> cameraInstructionText = new List<string>(new string[] { "FIND THE TOMATO!", "FIND THE ORANGE!", "FIND THE BANANAS!", "FIND THE APPLE!", "FIND THE EU FLAG TO REVOKE ARTICLE 50!" });
 
 
     private SyncListString activeButtonActions = new SyncListString();
@@ -239,7 +239,7 @@ public class InstructionController : NetworkBehaviour
                 }
                 else if (rand == 2)
                 {
-                    rand = UnityEngine.Random.Range(0, cameraInstructionText.Count);
+                    rand = UnityEngine.Random.Range(1, cameraInstructionText.Count);
                     RpcSetCameraPanel(i, rand, cameraInstructionText[rand]);
                 }
             }
